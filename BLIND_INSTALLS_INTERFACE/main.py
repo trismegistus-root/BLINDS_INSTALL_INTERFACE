add94bbace2ac937490549e990a05b455a1fcb98
Ecbiv <52392077+trismegistus-root@users.noreply.github.com>
2021-08-22 19:35:36 -0600

    Add files via upload

---
+++ BLIND_INSTALLS_INTERFACE/main.py
@@ -0,0 +1,8 @@
+from menu import Menu
+from sql_interface import SQL_INTERFACE
+from inputs import INPUT
+
+
+if __name__ == "__main__":
+    please = SQL_INTERFACE()
+    please.show_all().designers()
