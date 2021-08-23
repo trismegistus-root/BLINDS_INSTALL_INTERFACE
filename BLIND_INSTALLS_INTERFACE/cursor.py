0e4e9d42b7113484267a881b3b8ea0651b5cea7a
Ecbiv <52392077+trismegistus-root@users.noreply.github.com>
2021-08-22 20:38:53 -0600

    Update cursor.py

--- BLIND_INSTALLS_INTERFACE/cursor.py
+++ BLIND_INSTALLS_INTERFACE/cursor.py
@@ -1,13 +1,3 @@
 import pymysql as sql
 connection = sql.connect(host="localhost", user="root",passwd="", database="BLIND_INSTALLS")
 cursor = connection.cursor()
-
-"""
-InstallerTableSql = "SELECT * FROM INSTALLER;"
-cursor.execute(InstallerTableSql)
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-connection.commit()
-connection.close()
-"""
